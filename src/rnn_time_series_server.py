f59014ce9a51a0881feb450590229bbd05971306
Grzegorz Nowak <strange3studio@gmail.com>
2018-02-11 21:27:08 +0100

    test including modules using hacky wacky method

--- src/rnn_time_series_server.py
+++ src/rnn_time_series_server.py
@@ -6,6 +6,8 @@ def loadModelFromPath(absolutePath, moduleName, nameOfTheFactoryMethod):
     spec = importlib.util.spec_from_file_location(moduleName, absolutePath)
     foo = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(foo)
+    method_to_call = getattr(foo, nameOfTheFactoryMethod)
+    return method_to_call()
 
 #def loadModel(path):
 #    # loads model from a path
