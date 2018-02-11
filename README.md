9000587fbb4b388f2cf750ec44e39e044fc189d8
Grzegorz Nowak <strange3studio@gmail.com>
2018-02-11 21:00:46 +0100

    add an app scaffold with some bare tests etc.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # tensorflow-rrn-server
 Let's use flask to server some RNN time-series model
+
+
+## Installation
+
+`pip3 install flask`
+
+
+## Tests
+
+in the root of the project:
+
+`python3 src/tests/rnn_time_series_server_tests.py`
---
+++ src/tests/rnn_time_series_server_tests.py
@@ -0,0 +1,15 @@
+import unittest
+import requests
+
+
+class RNNTimeSeriesServerTestRequests(unittest.TestCase):
+
+    def test_random_response(self):
+        response = requests.get('http://localhost:5000/random')
+        self.assertIs(response.data, int)
+        rv = self.app.get('/random')
+        self.assertIs(rv.data, int)
+
+
+if __name__ == "__main__":
+    unittest.main()
