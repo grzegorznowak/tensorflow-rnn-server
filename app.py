1011ea465c298263fa177ba34ba0db0897985d8f
Grzegorz Nowak <strange3studio@gmail.com>
2018-03-04 23:26:28 +0100

    include sliding window scaling over data, minor mods, WIP but workable

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import tensorflow as tf
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
-
 sess      = tf.Session()
-new_saver = tf.train.import_meta_graph("/tmp/time_series/run-20180218152946-61-GRU_200_2_125_1_ADAM_MAE_NOLN_HIGHS/model_final_61.ckpt.meta")
-new_saver.restore(sess, tf.train.latest_checkpoint("/tmp/time_series/run-20180218152946-61-GRU_200_2_125_1_ADAM_MAE_NOLN_HIGHS"))
+# new_saver = tf.train.import_meta_graph("/tmp/time_series/run-20180225105446-47-GRU_500_3_125_1_ADAM_LOGITS_LN_ONEHOT/model_final_47.ckpt.meta")
+new_saver = tf.train.import_meta_graph("/tmp/time_series/run-20180304213917-224-RNN_200_1_62_1_ADAM_LOGITS_LN_ONEHOT/model_final_224.ckpt.meta")
+new_saver.restore(sess, tf.train.latest_checkpoint("/tmp/time_series/run-20180304213917-224-RNN_200_1_62_1_ADAM_LOGITS_LN_ONEHOT"))
 X_placeholder             = tf.get_default_graph().get_tensor_by_name("X:0")
-outputs_op                = tf.get_default_graph().get_tensor_by_name("Sigmoid:0")
-initial_state_placeholder = tf.get_default_graph().get_tensor_by_name("initial_state_placeholder:0")
+outputs_op                = tf.get_default_graph().get_tensor_by_name("outputs:0")
+initial_state_placeholder = tf.get_default_graph().get_tensor_by_name("PlaceholderWithDefault:0")
 is_training_placeholder   = tf.get_default_graph().get_tensor_by_name("is_training:0")
 keep_prob                 = tf.get_default_graph().get_tensor_by_name("keep_prob:0")
-final_state_op            = tf.get_default_graph().get_tensor_by_name("rnn/while/Exit_3:0")
-
+final_state_op_c            = tf.get_default_graph().get_tensor_by_name("rnn/while/Exit_3:0")
+final_state_op_h            = tf.get_default_graph().get_tensor_by_name("rnn/while/Exit_4:0")
 
 
 all_vars = tf.get_collection('vars')
@@ -37,29 +37,34 @@ def random():
 
 @app.route('/prediction')
 def predict():
-    current_observation  = rnn.ObservationData(rnn.raw_observation_to_list(request.args.get('observation')))
+    current_observation  = rnn.raw_observation_to_list(request.args.get('observation'))
+  #  try:
+     #   rnn.append_observation_to_db(current_observation)
+    prediction = rnn.make_prediction(current_observation, sess, outputs_op, initial_state_placeholder,
+                                     X_placeholder, is_training_placeholder, keep_prob, final_state_op_c, final_state_op_h)
 
-    try:
-        rnn.append_observation_to_db(current_observation)
-        prediction = rnn.make_prediction(sess, outputs_op, initial_state_placeholder,
-                                         X_placeholder, is_training_placeholder, keep_prob, final_state_op)
+    result = 0
+    print("prediction", prediction)
 
-        rounded_pred = np.around(prediction)
-        print(prediction)
-        output = 0;
-        if rounded_pred[0] == 1 and rounded_pred[1] == 0 and rounded_pred[2] == 0:
-            output = 1
-        elif rounded_pred[0] == 0 and rounded_pred[1] == 1 and rounded_pred[2] == 0:
-            output = -1
+    if prediction == 1:  # strong
+    #if 1 in prediction and 2 in prediction and 3 in prediction:  # strong
+        result = 3
+    if prediction == 2:
+      #if 4 in prediction and 5 in prediction and 6 in prediction:
+      result = -3
 
-        print(output)
-
-        return str(output)
+    if prediction == 3:  # soft
+       result = 1
+    if prediction == 4:
+        result = -1
 
 
-    except Exception as error:
-        print(error)
-        return str(error)
+    print(result)
+    return str(result)
+
+    # except Exception as error:
+    #     print(error)
+    #     return str(error)
     
     # return """we are {} observations away from updating the model; or something""".format(5-len(session['observations']))
     
--- src/rnn_time_series_server.py
+++ src/rnn_time_series_server.py
@@ -1,27 +1,30 @@
 import os
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 # TODO: this is just a stub of a proper backend storage (which we don't actually need at this stage).
 # all we need is a quick turnaround with a working PoC that will show us how good/bad evaluations from the model are
 database       = []
 next_rnn_state = None
+scaler         = None
+scaling_range  = 60
 
-BATCH_SIZE         = 125
-OBSERVATIONS_COUNT = 18
+BATCH_SIZE         = 62
+OBSERVATIONS_COUNT = 5
 STEPS_COUNT        = 1
 RNN_NEURONS        = 200  # this is a property of a model, need to be variablized somehow - rather hard, tbd.
-RNN_LAYERS         = 2    # this is a property of a model, need to be variablized somehow - rather hard, tbd.
-
-class ObservationData:
-    def __init__(self, list):
-        self.open    = list[0]
-        self.high    = list[1]
-        self.low     = list[2]
-        self.close   = list[3]
-        self.volume  = list[4]
-        self.time    = list[5]
-        self.control = list[6]
-
+RNN_LAYERS         = 1    # this is a property of a model, need to be variablized somehow - rather hard, tbd.
+#
+# class ObservationData:
+#     def __init__(self, list):
+#         self.open    = list[0]
+#         self.high    = list[1]
+#         self.low     = list[2]
+#         self.close   = list[3]
+#         self.volume  = list[4]
+#         self.time    = list[5]
+#         self.control = list[6]
+#
 
 def load_module_method_from_path(absolutePath, moduleName, nameOfTheFactoryMethod):
     import importlib.util
@@ -32,8 +35,10 @@ def load_module_method_from_path(absolutePath, moduleName, nameOfTheFactoryMetho
     return method_to_call
 
 
-def zero_state():
-    return np.random.randn(BATCH_SIZE, RNN_NEURONS * RNN_LAYERS)
+def zero_state(initial_state_op_c, initial_state_op_h):
+    return tuple(initial_state_op_c, initial_state_op_h)
+  #  return np.zeros((BATCH_SIZE, RNN_NEURONS * RNN_LAYERS))
+   # return np.random.randn(BATCH_SIZE, RNN_NEURONS * RNN_LAYERS)
 
 
 def raw_observation_to_list(observations_string_raw):
@@ -41,25 +46,25 @@ def raw_observation_to_list(observations_string_raw):
 
 
 def is_it_a_new_day(observation):
-    return observation.control == 0
+    return get_control_value_from_observation(observation) == 0
 
 
-def append_observation_to_db(observation):
-    global database
-    global next_rnn_state
-
-    if is_it_a_new_day(observation) or next_rnn_state is None:
-        database = []
-        next_rnn_state = zero_state()
-
-    else:
-        previous_observation = db_last_element()
-        if previous_observation is not None:  # which is possible if we went onto a new batch
-            validate_observations(previous_observation, observation)
-
-
-    database.append(observation)
-    return database
+# def append_observation_to_db(observation, initial_state_op_c, initial_state_op_h):
+#     global database
+#     global next_rnn_state
+#
+#     if is_it_a_new_day(observation) or next_rnn_state is None:
+#         database = []
+#         next_rnn_state = zero_state(initial_state_op_c, initial_state_op_h)
+#
+#     else:
+#         previous_observation = db_last_element()
+#         if previous_observation is not None:  # which is possible if we went onto a new batch
+#             validate_observations(previous_observation, observation)
+#
+#
+#     database.append(observation)
+#     return database
 
 
 def db_last_element():
@@ -81,38 +86,70 @@ def flush_db():
 
 """Need to get a feel of the data, if they make sense in general"""
 def validate_observations(observation_prev, observation_next):
-    print(observation_prev.control)
-    print(observation_next.control)
-    if observation_prev.control > observation_next.control:
+
+    if get_control_value_from_observation(observation_prev) > get_control_value_from_observation(observation_next):
         raise Exception('Wrong ordering of observation data')
-    if observation_next.control != observation_prev.control + 1:
+    if get_control_value_from_observation(observation_next) != get_control_value_from_observation(observation_prev) + 1:
         raise Exception('Observation data skipped an entry')
 
 
 """Calculate model response using data from db"""
-def make_prediction(sess, outputs_op, initial_state_placeholder,
-                    X_placeholder, is_training_placeholder, keep_prob, final_state_op):
+def make_prediction(current_observation, sess, outputs_op, initial_state_placeholder,
+                    X_placeholder, is_training_placeholder, keep_prob, final_state_op_c, final_state_op_h):
 
     global next_rnn_state
-    db = get_db()
-    db_len = len(db)
+    global database
+    global scaler
+    global scaling_range
 
-    observations  = maybe_fill_batch_with_sparse_vectors(unpack_labels(get_db()), BATCH_SIZE, STEPS_COUNT, OBSERVATIONS_COUNT)
-    outputs, new_states = sess.run([outputs_op, final_state_op], feed_dict={X_placeholder: observations,
-                                               keep_prob: 1,
-                                               initial_state_placeholder: next_rnn_state,
-                                               is_training_placeholder:False})
+    def execute_rnn(observation, initial_state):
+        Xs  = maybe_fill_batch_with_sparse_vectors(observation, BATCH_SIZE, STEPS_COUNT, OBSERVATIONS_COUNT)
+        if initial_state is None:
+            outputs, new_states_c, new_states_h = sess.run([outputs_op, final_state_op_c, final_state_op_h], feed_dict={X_placeholder: Xs,
+                                                                                keep_prob: 1,
+                                                                                is_training_placeholder:False})
+        else:
+            outputs, new_states_c, new_states_h = sess.run([outputs_op, final_state_op_c, final_state_op_h], feed_dict={X_placeholder: Xs,
+                                                                                    keep_prob: 1,
+                                                                                    initial_state_placeholder: initial_state,
+                                                                                    is_training_placeholder:False})
+        print("outputs: ",outputs[0][0])
+        #TODO: BATCH_SIZE must be configurable somewhere sometime
+        #reprime RNN state every BATCH_SIZE full data batches
 
-    # TODO: BATCH_SIZE must be configurable somewhere sometime
-    # reprime RNN state every BATCH_SIZE full data batches
-    if db_len == STEPS_COUNT:
 
-        next_rnn_state = new_states
-        flush_db()
-        print("db flushed")
+        return [[new_states_c, new_states_h]], outputs[0][0].argsort()[-1:][::-1]
+        #return  np.argmax(outputs[0][0])
 
-    return  outputs[0][0]
 
+    if is_it_a_new_day(current_observation):
+        database       = []
+        next_rnn_state = None
+        scaler         = None
+        return 0
+    else:
+        if len(database) > 0:
+            validate_observations(database[-1], current_observation)  # do not validate if a first sample for a day
+
+        database.append(current_observation)
+        if len(database) == scaling_range:
+            observations = np.array(drop_control_values(database))
+            scaler = MinMaxScaler((-1, 1))
+            scaler.fit(observations)
+            print(current_observation)
+            print(scaler.transform(drop_control_values([current_observation])))
+            database           = database[1:]  # drop first element so we scale over sliding window of `scaling_range` elements
+            next_state, result = execute_rnn(scaler.transform(drop_control_values([current_observation]))[0], next_rnn_state)
+            next_rnn_state     = next_state
+            return result
+        else: # not enough data in db to setup scaler and do any predictions
+            return 0
+
+
+
+"""return the same list without control values in it"""
+def drop_control_values(list):
+    return [list_item[:-1] for list_item in list]
 
 """Adds extra zero vectors up to batch_size"""
 def maybe_fill_batch_with_sparse_vectors(batch, batch_size, steps_count, observations_count):
@@ -121,9 +158,11 @@ def maybe_fill_batch_with_sparse_vectors(batch, batch_size, steps_count, observa
     return out_batch
 
 
-"""convert observation data back to arrays for convenience of the model"""
-def unpack_labels(observationDataList):
-    return np.array(list(map(lambda d: [d.open, d.high, d.low, d.close, d.volume, d.time], observationDataList)))
+def get_control_value_from_observation(observation):
+    return observation[-1]
+# """convert observation data back to arrays for convenience of the model"""
+# def unpack_labels(observationDataList):
+#     return np.array(list(map(lambda d: [d.open, d.high, d.low, d.close, d.volume, d.time], observationDataList)))
 
 # def make_a_prediction(previous_step_data):
 #     prediction_batch = create_a_data_batch(next_step_tensor)
